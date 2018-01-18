a296e356dfbac5380d9a74edf4e6eee985dec5be
miso999 <michal.simoncic@gmail.com>
2018-01-18 11:46:53 +0100

    Sticky header + active links + smooth scroll

---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,62 @@
+import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
+
+class StickyHeader {
+    constructor() {
+        this.header = $('.site-header');
+        this.triggerElement = $('.large-hero__title');
+        this.fixOnScroll();
+        this.pageSection = $('.page-section , .large-hero');
+        this.navLinks = $('.primary-nav a');
+        this.sectionWaypoint();
+        this.addSmoothScroll();
+    }
+
+    addSmoothScroll() {
+        this.navLinks.smoothScroll();
+    }
+
+    fixOnScroll() {
+        var config = this;
+        new Waypoint({
+            element: this.triggerElement[0],
+            handler: function () {
+                config.header.toggleClass('site-header--dark');
+            },
+        });
+    }
+
+    sectionWaypoint() {
+        var config = this;
+        this.pageSection.each(function () {
+            var currentSection = this;
+            new Waypoint({
+                element: currentSection,
+                handler: function (direction) {
+                    if (direction == 'down') {
+                        var pageLink = currentSection.getAttribute('data-section-link');
+                        config.navLinks.removeClass('active');
+                        if ($(window).scrollTop() > 400) {
+                            $(pageLink).addClass('active');
+                        }
+                    }
+                },
+                offset: "18%"
+            });
+            new Waypoint({
+                element: currentSection,
+                handler: function (direction) {
+                    if (direction == 'up') {
+                        var pageLink = currentSection.getAttribute('data-section-link');
+                        config.navLinks.removeClass('active');
+                        $(pageLink).addClass('active');
+                    }
+                },
+                offset: "-40%"
+            });
+        });
+
+    }
+}
+
+export default StickyHeader;
