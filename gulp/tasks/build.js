3f3e4b75923473a7f5fc42123f51282f8a49b173
miso999 <michal.simoncic@gmail.com>
2018-01-19 19:28:18 +0100

    build process finished

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     del = require('del'),
-    usemin = require('gulp-usemin');
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify');
 
 
 gulp.task('deleteDistFolder', function () {
     return del('./dist');
 });
 
-gulp.task('optimizeImages', ['deleteDistFolder'], function () {
+gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function () {
     return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
         .pipe(imagemin({
             progressive: true,
@@ -18,10 +21,13 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function () {
         .pipe(gulp.dest("./dist/assets/images"));
 });
 
-gulp.task('usemin', ['deleteDistFolder'], function () {
-return gulp.src('./app/index.html')
-    .pipe(usemin())
-    .pipe(gulp.dest('./dist'))
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function () {
+    return gulp.src('./app/index.html')
+        .pipe(usemin({
+            css: [function() {return rev()}, function() {return cssnano()}],
+            js: [function() {return rev()}, function() {return uglify()}]
+        }))
+        .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('build', ['deleteDistFolder', 'optimizeImages', 'usemin']);
